58f834153397dfc772fa3728e22783e1f4df69f2
vquyen96 <quyendvd00557@fpt.edu.vn>
2019-10-17 21:22:05 +0700

    create

---
+++ client/src/main/java/t1708e/webservice/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package t1708e.webservice.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+}
