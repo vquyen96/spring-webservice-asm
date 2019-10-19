9da62b6724714fae67b3da501d43f8411fa40d8e
Hungpvhd <hungpvhd00529@fpt.edu.vn>
2019-10-19 14:04:42 +0700

    Edit Client

--- client/src/main/resources/static/js/login.js
+++ client/src/main/resources/static/js/login.js
@@ -73,13 +73,11 @@
         if(showPass == 0) {
             $(this).next('input').attr('type','text');
             $(this).find('i').removeClass('zmdi-eye');
-            $(this).find('i').addClass('zmdi-eye-off');
             showPass = 1;
         }
         else {
             $(this).next('input').attr('type','password');
-            $(this).find('i').addClass('zmdi-eye');
-            $(this).find('i').removeClass('zmdi-eye-off');
+            $(this).find('i').addClass('material-icons');
             showPass = 0;
         }
 
