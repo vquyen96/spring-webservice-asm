8184a1265728cfd8d4764fe1d9066d1daffefc93
Hungpvhd <hungpvhd00529@fpt.edu.vn>
2019-10-21 00:11:15 +0700

    Create Admin Template

---
+++ client/src/main/java/t1708e/webservice/client/service/Category.java
@@ -0,0 +1,243 @@
+/**
+ * Category.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package t1708e.webservice.client.service;
+
+public class Category  implements java.io.Serializable {
+    private int id;
+
+    private String name;
+
+    private service.Place[] places;
+
+    private String status;
+
+    public Category() {
+    }
+
+    public Category(
+           int id,
+           String name,
+           service.Place[] places,
+           String status) {
+           this.id = id;
+           this.name = name;
+           this.places = places;
+           this.status = status;
+    }
+
+
+    /**
+     * Gets the id value for this Category.
+     * 
+     * @return id
+     */
+    public int getId() {
+        return id;
+    }
+
+
+    /**
+     * Sets the id value for this Category.
+     * 
+     * @param id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    /**
+     * Gets the name value for this Category.
+     * 
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+
+    /**
+     * Sets the name value for this Category.
+     * 
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    /**
+     * Gets the places value for this Category.
+     * 
+     * @return places
+     */
+    public service.Place[] getPlaces() {
+        return places;
+    }
+
+
+    /**
+     * Sets the places value for this Category.
+     * 
+     * @param places
+     */
+    public void setPlaces(service.Place[] places) {
+        this.places = places;
+    }
+
+    public service.Place getPlaces(int i) {
+        return this.places[i];
+    }
+
+    public void setPlaces(int i, service.Place _value) {
+        this.places[i] = _value;
+    }
+
+
+    /**
+     * Gets the status value for this Category.
+     * 
+     * @return status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+
+    /**
+     * Sets the status value for this Category.
+     * 
+     * @param status
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    private Object __equalsCalc = null;
+    public synchronized boolean equals(Object obj) {
+        if (!(obj instanceof Category)) return false;
+        Category other = (Category) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            this.id == other.getId() &&
+            ((this.name==null && other.getName()==null) || 
+             (this.name!=null &&
+              this.name.equals(other.getName()))) &&
+            ((this.places==null && other.getPlaces()==null) || 
+             (this.places!=null &&
+              java.util.Arrays.equals(this.places, other.getPlaces()))) &&
+            ((this.status==null && other.getStatus()==null) || 
+             (this.status!=null &&
+              this.status.equals(other.getStatus())));
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        _hashCode += getId();
+        if (getName() != null) {
+            _hashCode += getName().hashCode();
+        }
+        if (getPlaces() != null) {
+            for (int i=0;
+                 i<java.lang.reflect.Array.getLength(getPlaces());
+                 i++) {
+                Object obj = java.lang.reflect.Array.get(getPlaces(), i);
+                if (obj != null &&
+                    !obj.getClass().isArray()) {
+                    _hashCode += obj.hashCode();
+                }
+            }
+        }
+        if (getStatus() != null) {
+            _hashCode += getStatus().hashCode();
+        }
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(Category.class, true);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("http://service/", "category"));
+        org.apache.axis.description.ElementDesc elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("id");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "id"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "int"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("name");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "name"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("places");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "places"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://service/", "place"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(true);
+        elemField.setMaxOccursUnbounded(true);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("status");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "status"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+    }
+
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+    /**
+     * Get Custom Serializer
+     */
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           String mechType,
+           Class _javaType,
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanSerializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+    /**
+     * Get Custom Deserializer
+     */
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           String mechType,
+           Class _javaType,
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanDeserializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+}
