f4c84e2c07d165cad44ac35561a4cb26290804c8
Ngo Van Tuan <contact.tuannv@gmail.com>
2019-10-22 00:05:26 +0700

    generate salt password register

--- server/src/main/java/service/SignInService.java
+++ server/src/main/java/service/SignInService.java
@@ -4,12 +4,38 @@ import entity.User;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import util.HibernateUtil;
+import util.PasswordUtil;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 @WebService
 public class SignInService {
+
+    @WebMethod
+    public String login(String username, String password) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSession()) {
+            transaction = session.beginTransaction();
+            User user = findByUsername(username.toLowerCase().trim());
+
+            if (user != null) {
+                if (password == PasswordUtil.generateSecurePassword(user.getPassword(), user.getSalt())) {
+                    return "Login success.";
+                } else {
+                    return "Username or password invalid.";
+                }
+            } else {
+                return "Username or password invalid.";
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
 
     @WebMethod
     public User findByUsername(String username) {
