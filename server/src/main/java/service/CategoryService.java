2529e67fe6b714742e5115ed32785a4c2c04e4ee
minhnt <minhnt.contact@gmail.com>
2019-10-23 09:20:13 +0700

    add save category

--- server/src/main/java/service/CategoryService.java
+++ server/src/main/java/service/CategoryService.java
@@ -16,4 +16,66 @@ import java.util.logging.Logger;
 @WebService
 public class CategoryService {
 
+    @WebMethod
+    public void save(Category category) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSession()) {
+            transaction = session.beginTransaction();
+            session.saveOrUpdate(category);
+            transaction.commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+
+        }
+    }
+
+    @WebMethod
+    public List<Category> findAll() {
+        List<Category> categories = new ArrayList<>();
+        try (Session session = HibernateUtil.getSession()) {
+            categories = session.createQuery("from Category", Category.class).list();
+        } catch (Exception e) {
+
+        }
+        return categories;
+    }
+
+    @WebMethod
+    public Category findById(String id) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSession()) {
+            transaction = session.beginTransaction();
+            Category category = session.get(Category.class, id);
+            transaction.commit();
+            return category;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+
+            return null;
+        }
+    }
+
+    @WebMethod
+    public void delete(String id) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSession()) {
+            transaction = session.beginTransaction();
+            Category category = session.get(Category.class, id);
+            if (category != null) {
+                session.delete(category);
+
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            
+        }
+    }
 }
